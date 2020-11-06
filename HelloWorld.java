a526e1557b009f529474903103f8ce325c4652a3
Filipe Miranda <fmiranda@Filipes-MacBook-Pro-2.local>
2020-11-06 05:45:51 -0600

    new test

--- HelloWorld.java
+++ HelloWorld.java
@@ -23,7 +23,7 @@ public class HelloWorld {
             System.out.println("Request received from: " + t.getRemoteAddress().toString());
 
             // Displaying Hello message
-            String hello = "Test";
+            String hello = "Nov 6 5:45am";
             String response = "<html><body><h1>" + hello + "</h1>\n";
 
             // Displaying the OS arch that this is running on
